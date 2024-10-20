65ba1268ecfdca92dd5c6250bdea9e199c1161ab
tolstovr <tolstovrob@gmail.com>
2024-10-20 19:59:24 +0400

    Functions: shorter argument declaration

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import "fmt"
 
-func add(a int, b int) int {
+func add(a, b int) int { // a and b has same type, so we can shorten our code
 	return a + b
 }
 
+func printNum(sum int, prefix string) {
+	fmt.Println(prefix, sum)
+}
+
 func main() {
-	fmt.Println(add(3, 5))
+	printNum(add(40, 2), "Answer is:")
 }
