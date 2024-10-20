fd822ac0d7d4ddf758438e4780f281b0db53c5b0
tolstovr <tolstovrob@gmail.com>
2024-10-20 21:12:14 +0400

    Switch-case statement

--- main.go
+++ main.go
@@ -2,23 +2,19 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"runtime"
 )
 
-const EPS = 0.00000000000001
-
-func Sqrt(x float64) float64 {
-	z, prev := 1.0, 0.0
-
-	for math.Abs(z-prev) >= EPS {
-		prev = z
-		z -= (z*z - x) / (2 * z)
-	}
-
-	return z
-}
-
 func main() {
-	fmt.Println("Closest square to 49 is", Sqrt(49))
-	fmt.Println("Closest square to 50 is", Sqrt(50))
+	fmt.Print("Go runs on")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println(" macOS")
+	case "linux":
+		fmt.Println(" Linux")
+	case "windows":
+		fmt.Println("Windows")
+	default:
+		fmt.Printf("%s\n", os)
+	}
 }
