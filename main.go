e5b7d3117b77b1170b136bb4bfe18e8d337562ed
tolstovr <tolstovrob@gmail.com>
2024-10-20 20:34:31 +0400

    Constants

--- main.go
+++ main.go
@@ -3,11 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	v := 42           // type is int
-	new := v          // type is int as well
-	complex := 2 + 4i // type is complex128
+	const PI float64 = 3.14159
+	const E = 2.7182818284
 
-	fmt.Printf("Type of v is %T\n", v)
-	fmt.Printf("Type of new is %T\n", new)
-	fmt.Printf("Type of complex is %T\n", complex)
+	fmt.Println(PI, E)
 }
