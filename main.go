ee98821ea3f556ec085f79fb0d7a566c6ff9d70e
tolstovr <tolstovrob@gmail.com>
2024-10-20 20:33:11 +0400

    Type inference

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var x int = 3
-	z := float64(x)
+	v := 42           // type is int
+	new := v          // type is int as well
+	complex := 2 + 4i // type is complex128
 
-	fmt.Printf("Type of z is %T\n", z)
+	fmt.Printf("Type of v is %T\n", v)
+	fmt.Printf("Type of new is %T\n", new)
+	fmt.Printf("Type of complex is %T\n", complex)
 }
