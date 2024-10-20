eba9c2a7be8b03fe3a5ebaf2ae66fc02872ce5eb
tolstovr <tolstovrob@gmail.com>
2024-10-20 20:38:10 +0400

    For loop

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	const PI float64 = 3.14159
-	const E = 2.7182818284
-
-	fmt.Println(PI, E)
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
 }
