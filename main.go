0fac33da0a9447a4287a604f85c685fddedb0ad0
tolstovr <tolstovrob@gmail.com>
2024-10-20 20:41:41 +0400

    Infinite loops

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	sum := 1
-	for sum < 100 {
-		sum += sum
+	sum := 0
+
+	for {
+		sum += 1
+		fmt.Println(sum)
+
+		if sum >= 50 {
+			fmt.Println("OKAY!")
+			break
+		}
 	}
-	fmt.Println(sum)
 }
