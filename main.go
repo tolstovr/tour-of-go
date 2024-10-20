efad476b877a6e3d9af59de874b5410964ca7a99
tolstovr <tolstovrob@gmail.com>
2024-10-20 20:07:04 +0400

    Functions: named returns

--- main.go
+++ main.go
@@ -10,9 +10,17 @@ func swap(x, y int) (int, int) { // we return two values here, which can be assi
 	return y, x
 }
 
+func aboba(num int) (x, y int) { // values of x and y will be returned
+	x = num * 2
+	y = num * 3
+	return // same as return x, y
+}
+
 func main() {
 	a, b := 5, 10
 	fmt.Println("Before swapping:", a, b)
 	a, b = swap(a, b)
 	fmt.Println("After swapping:", a, b)
+
+	fmt.Println(aboba(50))
 }
