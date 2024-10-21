a167fc588b8d2491740818b6f8c71e5f420560bd
tolstovr <tolstovrob@gmail.com>
2024-10-21 15:01:20 +0400

    Structs

--- main.go
+++ main.go
@@ -2,11 +2,22 @@ package main
 
 import "fmt"
 
-func main() {
-	i := 42
+type Pos struct {
+	x, y int
+}
 
-	p := &i         // point to i
-	fmt.Println(*p) // get value from p
-	*p = 21
-	fmt.Println(i) // new value of i
+func main() {
+	fmt.Println(Pos{5, 3})
+
+	point := Pos{1, 2}
+	fmt.Println(point.x, point.y)
+	point.x = 10
+	fmt.Println(point.x, point.y)
+
+	p := &point
+	p.x = 5
+	fmt.Println(point.x, point.y)
+
+	axisPoint := Pos{x: 1} // y is unset => y = 0
+	fmt.Println(axisPoint.x, axisPoint.y)
 }
